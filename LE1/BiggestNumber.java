fefb192bcebbf368cd02b593443876d79d2e5b99
231B388 <231B388@juet.co.in>
2025-07-29 12:53:35 +0530

    added solution to lab ex 1

---
+++ LE1/BiggestNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BiggestNumber {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        if (a > b)
+            System.out.println(a);
+        else
+            System.out.println(b);
+        s.close();
+    }
+}
